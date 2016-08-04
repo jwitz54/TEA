c729da8c0a13b5b42d9053f74eb18442479f8571
Ubuntu <ubuntu@ip-172-31-10-92.ec2.internal>
2016-08-04 18:26:06 +0000

    trying to fix decrupt

--- tea.c
+++ tea.c
@@ -238,10 +238,13 @@ void encrypt (uint32_t* v, uint32_t* k, int data_size) {
 void decrypt (uint32_t* v, uint32_t* k, int data_size) {
 	int i;
 	for (i = 0; i < data_size*2; i += 2){
-	    uint32_t v0=v[i], v1=v[i + 1], sum=0xC6EF3720, i;  /* set up */
+	    uint32_t v0=v[i], v1=v[i + 1], sum=0xC6EF3720, w;  /* set up */
 	    uint32_t delta=0x9e3779b9;                     /* a key schedule constant */
-	    uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];   /* cache key */
-	    for (i=0; i<32; i++) {                         /* basic cycle start */
+	    uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];   /* cache key */	
+		if (i == 0){
+	    	printf("going into ref decryption v0: %i v1: %i k0: %i k1: %i k2: %i..\n", v0, v1, k0, k1, k2);
+	    }
+	    for (w=0; w<32; w++) {                         /* basic cycle start */
 	        v1 -= ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
 	        v0 -= ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
 	        sum -= delta;
